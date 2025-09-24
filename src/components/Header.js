6b34f55748d021b1579db0abc33e9f353bb95e69
AliefKaka <aliefkaka123@gmail.com>
2025-09-25 01:03:19 +0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-primary-green text-white py-6">
+      <div className="container mx-auto text-center">
+        <h1 className="text-4xl lg:text-5xl font-bold">Selamat Datang di Website Kelas</h1>
+        <h2 className="text-2xl mt-2 font-semibold">XI-D Kesehatan</h2>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
